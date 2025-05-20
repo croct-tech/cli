9da6b342dc2c0414270fffc91dba04c2faf27e24
Marcos Passos <marcos@croct.com>
2025-05-20 14:16:30 -0300

    Fix gitignore logic

--- src/application/predicate/matchesGitignore.ts
+++ src/application/predicate/matchesGitignore.ts
@@ -1,5 +1,5 @@
+import ignore from 'ignore';
 import {Predicate} from '@/application/predicate/predicate';
-import {MatchesGlob} from '@/application/predicate/matchesGlob';
 import {FileSystem} from '@/application/fs/fileSystem';
 import {WorkingDirectory} from '@/application/fs/workingDirectory/workingDirectory';
 
@@ -13,7 +13,7 @@ export class MatchesGitignore implements Predicate<[string]> {
 
     private readonly workingDirectory: WorkingDirectory;
 
-    private readonly cache: Map<string, MatchesGlob|null> = new Map();
+    private readonly cache: Map<string, Predicate<[string]>> = new Map();
 
     public constructor({fileSystem, workingDirectory}: Configuration) {
         this.fileSystem = fileSystem;
@@ -21,20 +21,41 @@ export class MatchesGitignore implements Predicate<[string]> {
     }
 
     public async test(path: string): Promise<boolean> {
-        return (await this.getPredicate())?.test(path) ?? false;
+        return (await this.getPredicate()).test(path);
     }
 
-    private async getPredicate(): Promise<MatchesGlob|null> {
-        const gitignorePath = this.fileSystem.joinPaths(this.workingDirectory.get(), '.gitignore');
+    private async getPredicate(): Promise<Predicate<[string]>> {
+        const workingDirectory = this.workingDirectory.get();
+        const gitignorePath = this.fileSystem.joinPaths(workingDirectory, '.gitignore');
 
         if (this.cache.has(gitignorePath)) {
             return this.cache.get(gitignorePath)!;
         }
 
-        let predicate: MatchesGlob|null = null;
+        let predicate: Predicate<[string]> = {
+            test: () => Promise.resolve(false),
+        };
 
         if (await this.fileSystem.exists(gitignorePath)) {
-            predicate = MatchesGlob.fromPattern(await this.fileSystem.readTextFile(gitignorePath));
+            const instance = ignore();
+
+            instance.add(await this.fileSystem.readTextFile(gitignorePath));
+
+            predicate = {
+                test: (path: string): Promise<boolean> => {
+                    if (!this.fileSystem.isSubPath(workingDirectory, path)) {
+                        return Promise.resolve(false);
+                    }
+
+                    const relativePath = this.fileSystem.getRelativePath(workingDirectory, path);
+
+                    if (relativePath === '') {
+                        return Promise.resolve(false);
+                    }
+
+                    return Promise.resolve(instance.ignores(relativePath));
+                },
+            };
         }
 
         this.cache.set(gitignorePath, predicate);
