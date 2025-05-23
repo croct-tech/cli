a06be18ca8dad8beacee697259733c7ead9a518a
Marcos Passos <marcospassos@users.noreply.github.com>
2025-05-23 16:35:45 -0300

    Add option to pass external token (#81)

---
+++ src/application/cli/authentication/authenticator/providedTokenAuthenticator.ts
@@ -0,0 +1,40 @@
+import {Token} from '@croct/sdk/token';
+import {Authenticator} from '@/application/cli/authentication/authenticator/index';
+import {ErrorReason, HelpfulError} from '@/application/error';
+
+export type Configuration = {
+    token: Token,
+};
+
+export class ProvidedTokenAuthenticator implements Authenticator<Record<never, never>> {
+    private readonly token: Token;
+
+    public constructor({token}: Configuration) {
+        this.token = token;
+    }
+
+    public getToken(): Promise<string|null> {
+        return Promise.resolve(this.token.toString());
+    }
+
+    public login(): Promise<string> {
+        this.reportUnsupportedOperation('login');
+    }
+
+    public logout(): Promise<void> {
+        this.reportUnsupportedOperation('logout');
+    }
+
+    private reportUnsupportedOperation(operation: 'login' | 'logout'): never {
+        throw new HelpfulError(
+            `${operation === 'login' ? 'Login' : 'Logout'} is not supported when using an externally provided token.`,
+            {
+                title: 'Unsupported operation',
+                reason: ErrorReason.PRECONDITION,
+                suggestions: [
+                    'Do not specify the `--token` option or the `CROCT_TOKEN` environment variable.',
+                ],
+            },
+        );
+    }
+}
