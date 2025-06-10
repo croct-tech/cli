fc480e4c7b828b797141bd2458a6f32398cfd151
Marcos Passos <marcos@croct.com>
2025-06-10 11:28:52 -0300

    Type narrowing

--- src/infrastructure/application/evaluation/functions.ts
+++ src/infrastructure/application/evaluation/functions.ts
@@ -3,7 +3,7 @@ import {randomUUID} from 'node:crypto';
 import {EvaluationError, GenericFunction} from '@/application/template/evaluation';
 import {HelpfulError} from '@/application/error';
 
-export const ext: GenericFunction = (path: JsonValue): JsonValue => {
+export const ext: GenericFunction = (path: JsonValue): string => {
     if (typeof path !== 'string') {
         throw new EvaluationError(
             'The `path` argument of the `ext` function must be a string, '
@@ -14,7 +14,7 @@ export const ext: GenericFunction = (path: JsonValue): JsonValue => {
     return path.split('.').pop() ?? '';
 };
 
-export const basename: GenericFunction = (path: JsonValue, omitExtension: JsonValue = false): JsonValue => {
+export const basename: GenericFunction = (path: JsonValue, omitExtension: JsonValue = false): string => {
     if (typeof path !== 'string') {
         throw new EvaluationError(
             'The `path` argument of the `basename` function must be a string, '
@@ -38,7 +38,7 @@ export const basename: GenericFunction = (path: JsonValue, omitExtension: JsonVa
     return result;
 };
 
-export const dirname: GenericFunction = (path: JsonValue): JsonValue => {
+export const dirname: GenericFunction = (path: JsonValue): string => {
     if (typeof path !== 'string') {
         throw new EvaluationError(
             'The `path` argument of the `dirname` function must be a string, '
@@ -49,4 +49,4 @@ export const dirname: GenericFunction = (path: JsonValue): JsonValue => {
     return path.replace(/[\\/][^\\/]*$/, '');
 };
 
-export const uuid: GenericFunction = (): JsonValue => randomUUID();
+export const uuid: GenericFunction = (): string => randomUUID();
